d1d8c14729152a9d8e62c5db866a7d85e14a63df
zzp <zhangzpp@mail.ustc.edu.cn>
2015-06-01 21:12:04 +0800

    add remove element solution

---
+++ RemoveElement/Solution.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int removeElement(int[] nums, int val) {
+        int index=0;
+        if (nums.length == 0)
+            return 0;
+
+
+        for (int i=0; i<nums.length; i++) {
+            if(nums[i] != val) {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+
+        return index;
+    }
+}
