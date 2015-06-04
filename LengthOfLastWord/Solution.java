814184b642ef2e74cac3d58306fbf8d36490eb7e
zzp <zhangzpp@mail.ustc.edu.cn>
2015-06-04 11:37:15 +0800

    add length of last word solution

---
+++ LengthOfLastWord/Solution.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public int lengthOfLastWord(String s) {
+        if (s == null)
+            return 0;
+        s = s.trim();
+        if (s.length() == 0)
+            return 0;
+        int len=0;
+        for (int i=s.length()-1; i >=0; i--) {
+            if (s.charAt(i) != ' ')
+                len++;
+            else
+                break;
+        }
+
+        return len;
+    }
+}
